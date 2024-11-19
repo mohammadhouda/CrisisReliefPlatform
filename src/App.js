42b0c9c704dc66f87ae2bfeb8e1351e0a6237f22
mo <mo@gmail.com>
2024-11-20 00:53:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header bg-red-600 py-5 text-white font-semibold text-2xl text-center">
+        THIS IS A TEST NIGGA
+      </header>
     </div>
   );
 }
